08cb43ea6b5f5ed7c34b6fa3522e5e9e45139e6a
yang-ly <695954538@qq.com>
2019-07-15 20:38:11 +0800

    delete 1.java add Demo.java

---
+++ com/offcn/text/Demo.java
@@ -0,0 +1,8 @@
+package com.offcn.text;
+
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        System.out.println("你好呀");
+    }
+}
