4993c72099a598437b97c4826d379253a350f8ed
yang-ly <52533676+yang-ly@users.noreply.github.com>
2019-07-15 22:41:42 +0800

    Update Demo.java

--- com/offcn/text/Demo.java
+++ com/offcn/text/Demo.java
@@ -5,5 +5,6 @@ public class Demo {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("你好呀");
+        System.out.println("哈哈哈");
     }
 }
